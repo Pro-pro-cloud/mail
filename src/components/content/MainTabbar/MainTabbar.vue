98698dec1f8094f8cdd2dd3da7d28d7158427c82
Pro-pro-cloud <Pro-pro-cloud>
2021-04-16 21:19:56 +0800

    nab-bar-1

--- src/components/content/MainTabbar/MainTabbar.vue
+++ src/components/content/MainTabbar/MainTabbar.vue
@@ -1,21 +1,21 @@
 <template>
       <tabbar>
-      <TabbarItem path="/home" activeColor="red">
+      <TabbarItem path="/home">
         <img slot="item-icon" src="../../../assets/img/tabbar/home.svg" alt="">
         <img slot="item-icon-active" src="../../../assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-text">首页</div>
       </TabbarItem>
-      <TabbarItem path="/categery" activeColor="red">
+      <TabbarItem path="/categery">
         <img slot="item-icon" src="../../../assets/img/tabbar/category.svg" alt="">
         <img slot="item-icon-active" src="../../../assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-text">分类</div>
       </TabbarItem>
-      <TabbarItem path="/cart" activeColor="pink">
+      <TabbarItem path="/cart">
         <img slot="item-icon" src="../../../assets/img/tabbar/shopcart.svg" alt="">
         <img slot="item-icon-active" src="../../../assets/img/tabbar/shopcart_active.svg" alt="">
         <div slot="item-text">购物车</div>
       </TabbarItem>
-      <TabbarItem path="/profile" activeColor="blue">
+      <TabbarItem path="/profile">
         <img slot="item-icon" src="../../../assets/img/tabbar/profile.svg" alt="">
         <img slot="item-icon-active" src="../../../assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-text">我的</div>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,23 @@
 <template>
-<h3>home</h3>
+  <div id="home">
+    <navbar class="home-nav"><div slot="center">购物街</div></navbar>
+  </div>
 </template>
 
 <script>
+import Navbar from '../../components/common/navbar/navbar'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
