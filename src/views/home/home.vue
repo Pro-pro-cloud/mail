899ae03d210210995285ab6419bcbed13a255051
Pro-pro-cloud <Pro-pro-cloud>
2021-04-19 18:51:05 +0800

    swiper-bug-1

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="home">
     <navbar class="home-nav"><div slot="center">购物街</div></navbar>
+    <swiper>
+      <SwiperItem v-for="(item,index) in banners" :key="index">
+        <a :href="item.link"></a>
+        <img :src="item.image">
+      </SwiperItem>
+    </swiper>
   </div>
 </template>
 
 <script>
 import Navbar from '../../components/common/navbar/navbar'
 import { getHomeMultidata } from '../../network/home'
+import { Swiper, SwiperItem } from '../../components/swiper'
 
 export default {
   name: 'Home',
   components: {
-    Navbar
+    Navbar,
+    Swiper,
+    SwiperItem
   },
   data () {
     return {
@@ -24,6 +33,7 @@ export default {
     getHomeMultidata().then((res) => {
       this.banners = res.data.banner.list
       this.recommends = res.data.recommend.list
+      console.log(this.banners)
     })
   }
 }
